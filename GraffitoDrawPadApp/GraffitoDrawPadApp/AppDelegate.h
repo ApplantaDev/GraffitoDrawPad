41284f791890d6ad11e88d54514be27cf9f815e5
Rahiem Klugh <rjk@touchcorelogic.com>
2015-08-17 20:06:20 -0400

    Created New Xcode Project
    Created a new Xcode project for GraffitoDrawPad

---
+++ GraffitoDrawPadApp/GraffitoDrawPadApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GraffitoDrawPadApp
+//
+//  Created by Rahiem Klugh on 8/17/15.
+//  Copyright (c) 2015 TouchCore Logic, LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
